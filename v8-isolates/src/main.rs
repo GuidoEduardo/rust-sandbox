f8f851038add89e23a873aca77e8e548cf213df4
Guido Eduardo <guidoeduardonsilva@gmail.com>
2023-10-23 23:20:32 -0300

    feat: testing v8 isolates

---
+++ v8-isolates/src/main.rs
@@ -0,0 +1,22 @@
+use v8;
+
+fn main() {
+    let platform = v8::new_default_platform(0, false).make_shared();
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+
+    let isolate = &mut v8::Isolate::new(Default::default());
+
+    let scope = &mut v8::HandleScope::new(isolate);
+    let context = v8::Context::new(scope);
+    let scope = &mut v8::ContextScope::new(scope, context);
+
+    let code = v8::String::new(scope, "100/2").unwrap();
+
+    let script = v8::Script::compile(scope, code, None).unwrap();
+
+    let result = script.run(scope).unwrap();
+    let result = result.to_string(scope).unwrap();
+
+    assert!(result.to_rust_string_lossy(scope) == "50");
+}
